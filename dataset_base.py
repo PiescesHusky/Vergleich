67494046cb587877e84eea44ddbdb2b946a534a4
PiescesHusky <547104333@qq.com>
2019-04-24 05:21:54 +0800

    Add files via upload

--- dataset_base.py
+++ dataset_base.py
@@ -62,7 +62,7 @@ class DatasetBase(torchtext.data.Dataset):
             return [], [], -1
 
         split_tokens = [token.split(u"￨") for token in tokens]
-#        split_tokens = [token.split(u"|") for token in tokens]
+     #   split_tokens = [token.split(u"|") for token in tokens]
         split_tokens = [token for token in split_tokens if token[0]]
         token_size = len(split_tokens[0])
 
